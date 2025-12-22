b1f4c9477883fb04913e17465a0661ac58306808
MaxChip101 <1max0lag1@gmail.com>
2025-12-22 00:32:12 -0500

    made functions for everything

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,12 @@
 const std = @import("std");
 const os = std.os;
 const linux = os.linux;
+const io = std.io;
 const fs = std.fs;
+const mem = std.mem;
 
-var stdout = std.io.getStdOut().writer();
-var stdin = std.io.getStdIn().reader();
+var stdout = io.getStdOut().writer();
+var stdin = io.getStdIn().reader();
 
 pub fn main() !void {
     const tty_file = try fs.openFileAbsolute("/dev/tty", .{});
@@ -33,7 +35,7 @@ pub fn main() !void {
     new_settings.cc[@intFromEnum(linux.V.TIME)] = 0;
     new_settings.cc[@intFromEnum(linux.V.MIN)] = 1;
 
-    _ = linux.tcsetattr(tty_fd, linux.TCSA.NOW, &new_settings);
+    _ = linux.tcsetattr(tty_fd, linux.TCSA.FLUSH, &new_settings);
 
     try stdout.writeAll("\x1B[?25l"); // Hide the cursor.
     try stdout.writeAll("\x1B[s"); // Save cursor position.
@@ -46,18 +48,77 @@ pub fn main() !void {
         var input_buffer: [1]u8 = undefined;
         _ = try tty_file.read(&input_buffer);
 
-        if (input_buffer[0] == 'q') {
-            _ = linux.tcsetattr(tty_fd, linux.TCSA.NOW, &old_settings);
-            try stdout.writeAll("\x1B[?1049l"); // Disable alternative buffer.
-            try stdout.writeAll("\x1B[?47l"); // Restore screen.
-            try stdout.writeAll("\x1B[u"); // Restore cursor position.
-            return;
-        }
+        switch (input_buffer[0]) {
+            'q' => {
+                _ = linux.tcsetattr(tty_fd, linux.TCSA.FLUSH, &old_settings);
+                try stdout.writeAll("\x1B[?1049l"); // Disable alternative buffer.
+                try stdout.writeAll("\x1B[?47l"); // Restore screen.
+                try stdout.writeAll("\x1B[u"); // Restore cursor position.
+                return;
+            },
+            '\x1B' => {
+                new_settings.cc[@intFromEnum(linux.V.TIME)] = 1;
+                new_settings.cc[@intFromEnum(linux.V.MIN)] = 0;
+                _ = linux.tcsetattr(tty_fd, linux.TCSA.NOW, &new_settings);
 
-        try stdout.print("typed: ({c})\r\n", .{input_buffer[0]});
+                var escape_buffer: [8]u8 = undefined;
+                const escape_read = try tty_file.read(&escape_buffer);
+
+                new_settings.cc[@intFromEnum(linux.V.TIME)] = 0;
+                new_settings.cc[@intFromEnum(linux.V.MIN)] = 1;
+                _ = linux.tcsetattr(tty_fd, linux.TCSA.NOW, &new_settings);
+
+                if (escape_read == 0) {
+                    try stdout.print("typed: (escape)\r\n", .{});
+                } else if (mem.eql(u8, escape_buffer[0..escape_read], "[A")) {
+                    try stdout.print("typed: (up arrow)\r\n", .{});
+                } else if (mem.eql(u8, escape_buffer[0..escape_read], "[B")) {
+                    try stdout.print("typed: (down arrow)\r\n", .{});
+                } else if (mem.eql(u8, escape_buffer[0..escape_read], "[C")) {
+                    try stdout.print("typed: (right arrow)\r\n", .{});
+                } else if (mem.eql(u8, escape_buffer[0..escape_read], "[D")) {
+                    try stdout.print("typed: (left arrow)\r\n", .{});
+                } else if (mem.eql(u8, escape_buffer[0..escape_read], "a")) {
+                    try stdout.print("typed: (alt-a)\r\n", .{});
+                } else {
+                    try stdout.print("typed: (unkown escape sequence)\r\n", .{});
+                }
+            },
+            else => {
+                try stdout.print("typed: ({c})\r\n", .{input_buffer[0]});
+            },
+        }
     }
 }
 
 fn moveCursor(writer: anytype, row: usize, col: usize) !void {
-    _ = try writer.print("\x1B[{};{}H", .{ row + 1, col + 1 });
+    try writer.print("\x1B[{};{}H", .{ row + 1, col + 1 });
+}
+
+fn hideCursor(writer: anytype) !void {
+    try writer.writeAll("\x1B[?25l");
+}
+
+fn restoreCursor(writer: anytype) !void {
+    try writer.writeAll("\x1B[u");
+}
+
+fn saveCursor(writer: anytype) !void {
+    try writer.writeAll("\x1B[s");
+}
+
+fn alternateBuffer(writer: anytype) !void {
+    try writer.writeAll("\x1B[?1049h");
+}
+
+fn originalBuffer(writer: anytype) !void {
+    try writer.writeAll("\x1B[?1049l");
+}
+
+fn restoreScreen(writer: anytype) !void {
+    try writer.writeAll("\x1B[?47l");
+}
+
+fn saveScreen(writer: anytype) !void {
+    try writer.writeAll("\x1B[?47h");
 }
