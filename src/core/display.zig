36de245aea91d919e120155b29b0e6527df56a76
MaxChip101 <1max0lag1@gmail.com>
2026-01-18 22:19:46 -0500

    working more on buffers to display

--- src/core/display.zig
+++ src/core/display.zig
@@ -1,21 +1,11 @@
 const std = @import("std");
 const fs = std.fs;
+const mem = std.mem;
 
 const Buffer = struct {
-    var currentLine: u32 = 0;
-    var currentChar: u32 = 0;
     var lines: []Line = undefined;
 
-    pub fn getCurrentLine() !Line {
-        if (currentLine > lines.len) {
-            return error.OUT_OF_BOUNDS;
-        }
-        return lines[currentLine];
-    }
-
-    pub fn getCurrentChar() !u8 {
-        return try getCurrentLine()[currentChar];
-    }
+    pub fn init(allocator: mem.Allocator, rows: u16, columns: u16) !void {}
 };
 
 const Line = struct {
